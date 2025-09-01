9883914d24de61d77bdff36482a62dda15023d49
Shubham807621 <shubhammusale111@gmail.com>
2025-09-01 18:55:34 +0530

    updated site basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import ProductPage from './Pages/Product/productpage';
+
+
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-     <div className="App">
-      <ProductPage />
-    </div>
+  
       
     </>
   )

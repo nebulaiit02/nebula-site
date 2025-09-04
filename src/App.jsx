d0d856b6194d5a1e6343fdac77a35ec44b223a91
Shubham807621 <shubhammusale111@gmail.com>
2025-09-04 17:35:09 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
-import ProductsPage from "./Pages/Product/ProductPage";
-import ContactPage from "./Pages/Contact/ContactPage";
-import ProductDetailsPage from "./Pages/Product/Details/ProductDetailPage";
+import ProductsPage from "./pages/Product/ProductPage";
+import ContactPage from "./pages/Contact/ContactPage";
+import ProductDetailsPage from "./pages/Product/Details/ProductDetailPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./App.css";
 import ServicePage from "./pages/Service/ServicePage";
 import HomePage from "./pages/Home/HomePage";
 import AboutPage from "./Pages/AboutUs/About";
+import "./App.css";
 
 function App() {
   return (
@@ -20,7 +20,7 @@ function App() {
         <Route path="/contact-us" element={<ContactPage />} />
         <Route path="/product/:id" element={<ProductDetailsPage />} />
         <Route path="/service" element={<ServicePage />} />
-        <Route path="/About-us" element={<AboutPage />} />
+        <Route path="/About-us  " element={<AboutPage />} />
       </Routes>
       <Footer />
     </BrowserRouter>

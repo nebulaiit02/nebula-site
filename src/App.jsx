27d527eb5858c144584ed4c2e60a4bba0458a74b
Shubham807621 <shubhammusale111@gmail.com>
2025-09-04 17:51:59 +0530

    Update App jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
-import ContactPage from "./pages/Contact/ContactPage";
 import ProductDetailPage from "./pages/Product/Details/ProductDetailPage";
 import ProductsPage from "./pages/Product/ProductsPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ServicePage from "./pages/Service/ServicePage";
 import HomePage from "./pages/Home/HomePage";
 import AboutPage from "./Pages/AboutUs/About";
+import ContactPage from './pages/Contact/ContactPage'
 import "./App.css";
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/product-page" element={<ProductsPage />} />
-        <Route path="/contact-us" element={<ContactPage />} />
+        <Route path="/contact-us" element={<ContactPage/>} />
         <Route path="/product/:id" element={<ProductDetailPage />} />
         <Route path="/service" element={<ServicePage />} />
         <Route path="/About-us  " element={<AboutPage />} />
